42dbcc416f0a16cb0014af23ac741be3047fcba1
Richard Monaghan <Richard_Monaghan@hotmail.com>
2020-09-18 20:20:31 -0500

    Added Item bag scrape and formating changes

--- Equipped_Clean.py
+++ Equipped_Clean.py
@@ -11,30 +11,38 @@ payload = {
 
 
 def ItemGrab():
-    Gods = []
-    CurrentSpawn = []
-    with requests.Session() as s:
-        p = s.post('https://sigil.outwar.com/login', data=payload)
-        me = s.get('http://sigil.outwar.com/myaccount')
-        soup = BeautifulSoup(me.content, 'html.parser')
-        for Accounts in soup.findAll('a', attrs={'href': re.compile("^http://")}):
-            Account = Accounts.get('href')
-            AccountName = Accounts.get('target')
-            pattern = 'PLAY!'
-            result = re.match(pattern, AccountName)
-            if Accounts.text != "PLAY!":
-                me = s.get(Account)
-                #print(Account)
-                print(Accounts.text + " Is wearing ")
-                print("-------------------------------")
-                Equipped = s.get('https://sigil.outwar.com/equipment')
-                Equipsoup = BeautifulSoup(Equipped.content, 'html.parser')
-                for a in Equipsoup.find_all('img'):
-                    print(a['alt'])
-                print("--------------------------------")
-            else:
-                pass
+	Gods = []
+	CurrentSpawn = []
+	with requests.Session() as s:
+		p = s.post('https://sigil.outwar.com/login', data=payload)
+		me = s.get('http://sigil.outwar.com/myaccount')
+		soup = BeautifulSoup(me.content, 'html.parser')
+		table = soup.find( "table", {"id":"characterTable"} )
+		for Accounts in table.findAll('a', attrs={'href': re.compile("^http://")}):
+			Account = Accounts.get('href')
+			AccountName = Accounts.get('target')
+			pattern = 'PLAY!'
+			result = re.match(pattern, AccountName)
+			if Accounts.text != "PLAY!":
+				me = s.get(Account)
+				print("######################################################")
+				print(Accounts.text + " Is wearing ")
+				print("######################################################")
+				Equipped = s.get('https://sigil.outwar.com/equipment')
+				Bags = s.get('https://sigil.outwar.com/backpack')
+				Equipsoup = BeautifulSoup(Equipped.content, 'html.parser')
+				Bagsoup = BeautifulSoup(Bags.content, 'html.parser')
+				for a in Equipsoup.find_all('img'):
+					print(a['alt'])
+					print("--------------------------------")
+				print("##########################")
+				print("       Bags")
+				print("##########################")
+				for b in Bagsoup.find_all('img', alt=True):
+					print(b['alt'])
+					print("--------------------------------")
+				else:
+					pass
+
 
 ItemGrab()
-
-
